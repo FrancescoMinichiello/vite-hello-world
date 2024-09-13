66993b346d510aa17c32e48b1d9263b176d7c9b7
FrancescoMinichiello <fraps2278@gmail.com>
2024-09-13 17:38:33 +0200

    esercizio finito con bonus

---
+++ src/components/appHero.vue
@@ -0,0 +1,25 @@
+<script>
+export default{
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="jumbotron d-flex justify-content-center">
+        <h2>
+            Jumbotron
+        </h2>
+    </div>
+</template>
+
+<style scoped>
+    .jumbotron{
+        width: 1200px;
+        height: 500px;
+        background-color: aqua;
+    }
+</style>
